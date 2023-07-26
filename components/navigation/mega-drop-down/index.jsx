2aa1505a74261ffe647e02bb7f34aa4a531f9931
Ruhul Amin <ruhulinfo50@gmail.com>
2023-07-26 22:12:03 +0600

    render data in mage dropdown

--- components/navigation/mega-drop-down/index.jsx
+++ components/navigation/mega-drop-down/index.jsx
@@ -1,23 +1,42 @@
 import Image from "next/image";
+import Link from "next/link";
+import PropTypes from "prop-types";
 
 function MegaDropDown({ dropDownData }) {
   return (
-    <section className="min-w-[400px] columns-2 space-y-5 rounded-b-md border bg-white p-4">
+    <section className="min-w-[25rem] columns-2 space-y-5 rounded-b-md border bg-white p-4">
       {dropDownData.map((dropDown) => (
-        <div className="flex items-center gap-4">
-          <div className="relative h-[40px] w-[40px] shrink-0">
-            <Image src="/assets/images/deluxe.jpeg" alt="store-image" fill />
-          </div>
-          <p>Deluxe Online</p>
+        <div key={dropDown.slug}>
+          <Link href={`/${dropDown.slug}`}>
+            <div className="flex items-center gap-4">
+              <div className="relative h-[2.5rem] w-[2.5rem] shrink-0">
+                <Image src={dropDown.imageUrl} alt="store-image" fill />
+              </div>
+              <p>{dropDown.value}</p>
+            </div>
+          </Link>
         </div>
       ))}
-
-      <div className="flex items-center gap-4">
-        <p>View All</p>
-        <div className="relative h-[40px] w-[40px] shrink-0"></div>
+      <div>
+        <Link href="/">
+          <div className="flex items-center gap-4">
+            <p>View All</p>
+            <div className="relative h-[2.5rem] w-[2.5rem] shrink-0"></div>
+          </div>
+        </Link>
       </div>
     </section>
   );
 }
+
+MegaDropDown.propTypes = {
+  dropDownData: PropTypes.arrayOf(
+    PropTypes.shape({
+      slug: PropTypes.string,
+      value: PropTypes.string,
+      imageUrl: PropTypes.string,
+    })
+  ),
+};
 
 export default MegaDropDown;
--- utils/data/store-data.js
+++ utils/data/store-data.js
@@ -21,22 +21,22 @@ export const storeList = [
   },
 
   {
-    slug: "deluxe-online",
+    slug: "deluxe-online-1",
     value: "Deluxe Online",
     imageUrl: "/assets/images/deluxe.jpeg",
   },
   {
-    slug: "easy-electronic",
+    slug: "easy-electronic-1",
     value: "Easy Electronic",
     imageUrl: "/assets/images/deluxe.jpeg",
   },
   {
-    slug: "b-store",
+    slug: "b-store-1",
     value: "B Store",
     imageUrl: "/assets/images/deluxe.jpeg",
   },
   {
-    slug: "tech-shop",
+    slug: "tech-shop-1",
     value: "Tech Shop",
     imageUrl: "/assets/images/deluxe.jpeg",
   },
