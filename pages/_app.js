d8ff756f0129a5de9d3f0f1ca32eae059d0f9520
Ruhul Amin <ruhulinfo50@gmail.com>
2023-07-27 08:07:41 +0600

    config react query

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
   );
 }
