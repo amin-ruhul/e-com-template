9f85bbf445b5e43c660d437f34fb96c2306cc8df
Ruhul Amin <ruhulinfo50@gmail.com>
2023-07-27 06:18:50 +0600

    Add main grid

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 import Layout from "@/components/layout";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="flex min-h-[50vh] justify-center bg-secondary">
+      <div className="container grid bg-white lg:grid-cols-4">
+        <div className="hidden bg-red-200 lg:block">sidebar</div>
+        <div className="col-span-2">Slider</div>
+        <div className="hidden bg-blue-200 lg:block">Promo banner</div>
+      </div>
+    </div>
+  );
 }
