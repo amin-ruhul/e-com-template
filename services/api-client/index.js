b99aa3023332c72bd34a8787a68dc52b248c176c
Ruhul Amin <ruhulinfo50@gmail.com>
2023-07-27 08:00:56 +0600

    Add api client

---
+++ services/api-client/index.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_MOCK_URL,
+
+  timeout: 20000,
+});
+
+apiClient.interceptors.request.use(
+  function (config) {
+    /*
+     TODO: set access token
+     config.headers.Authorization = `Bearer ${getAccessToken()}`
+     */
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+apiClient.interceptors.response.use(
+  function (response) {
+    return {
+      data: response.data,
+      status: response.status,
+      statusText: response.statusText,
+    };
+  },
+  function (error) {
+    /*
+      TODO: clear token if status code is unauthorize
+    */
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
